cdc46fe3072ff42135760f5a64798fd90d17cb8c
eivydasaleksa <ealeksa121@gmail.com>
2022-03-20 11:39:56 +0200

    Project

---
+++ src/main/java/HibernateUtil.java
@@ -0,0 +1,46 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate?serverTimezone=UTC");
+                properties.put(Environment.USER, "root");
+                properties.put(Environment.PASS, "ealeksa121");
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "create");
+
+                configuration.setProperties(properties);
+
+//                configuration.addAnnotatedClass(Owner.class);
+//                configuration.addAnnotatedClass(Book.class);
+//                configuration.addAnnotatedClass(Library.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+    public void closeSessionFactory(){
+        sessionFactory.close();
+    }
+}
